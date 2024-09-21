311922f3cc755abff029d64484a1747633cc52bf
Jonathan Ouyang <jonsouyang@gmail.com>
2024-09-20 23:00:13 -0700

    added main body sections

--- ucla-website/src/App.tsx
+++ ucla-website/src/App.tsx
@@ -1,15 +1,17 @@
 import Banner from "./BannerComponents/Banner";
 import Sidebar from "./SidebarComponents/Sidebar";
+import MainBody from "./MainComponents/MainBody";
 import styles from "./App.module.css";
 
 //<div className={`${styles.item} ${styles.bannerStyle}`}></div>
+//<div className={`${styles.item} ${styles.mainBody}`}>Main Body</div>
 function App() {
   return (
     <>
       <div className={styles.background}>
         <Banner iconText="Hello Bruin!"/>
         <Sidebar />
-        <div className={`${styles.item} ${styles.mainBody}`}>Main Body</div>
+        <MainBody />
       </div>
     </>
   );
---
+++ ucla-website/src/MainComponents/MainBody.tsx
@@ -0,0 +1,14 @@
+import styles from "./MainBody.module.css";
+
+const MainBody = () => {
+  return (
+    <div>
+      <div className={styles.yearTitle}>Block 1</div>
+      <div className={styles.yearTitle2}>Block 2</div>
+      <div className={styles.yearTitle}>Block 3</div>
+      <div className={styles.yearTitle2}>Block 4</div>
+    </div>
+  );
+};
+
+export default MainBody;
